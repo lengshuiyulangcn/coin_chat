471b31902371057cfcc38d1e68f0e7a07590fadf
akidan <zhiyue7@gmail.com>
2018-04-17 20:34:18 +0900

    add cms

--- index.js
+++ index.js
@@ -22,12 +22,8 @@ var reset   = '\u001b[0m';
         for(let i=0; i < chats.length; i++) {
           console.log(blue + chats[i].name + ": " + cyan + chats[i].content + reset)
         }
-        request('https://coincheck.com/api/rate/xrp_jpy').then(price=>{ console.log(blue+"XRP: " + JSON.parse(price).rate+reset)})
-        request('https://coincheck.com/api/rate/bch_jpy').then(price=>{ console.log(blue+"BCH: " + JSON.parse(price).rate+reset)})
-        request('https://coincheck.com/api/rate/lsk_jpy').then(price=>{ console.log(blue+"LSK: " + JSON.parse(price).rate+reset)})
         request('https://api.zaif.jp/api/1/last_price/zaif_jpy').then(last_price=>{ console.log(blue+"ZFT: "+ JSON.parse(last_price).last_price+reset)})
-        request('https://api.zaif.jp/api/1/last_price/xem_jpy').then(last_price=>{ console.log(blue+"NEM: "+ JSON.parse(last_price).last_price+reset)})
-        request('https://api.zaif.jp/api/1/last_price/mona_jpy').then(last_price=>{ console.log(blue+"MNA: "+ JSON.parse(last_price).last_price+reset)})
+        request('https://api.zaif.jp/api/1/last_price/mosaic.cms_jpy').then(last_price=>{ console.log(blue+"CMS: "+ JSON.parse(last_price).last_price+reset)})
       } catch(e) {
       }
     })
